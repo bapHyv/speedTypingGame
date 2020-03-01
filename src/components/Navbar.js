bb44c5049a1c19aaa7668d78396b57dbdac2dc91
baphyv <baptiste.hyver@gmail.com>
2020-03-01 18:29:05 +0100

    created components

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div>
+            <ul>
+                <li>Speed typing game</li>
+                <li>My score</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar
---
+++ src/pages/ScorePage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Score from '../components/Score'
+
+const ScorePage = () => {
+    return (
+        <div>
+            <Score />
+        </div>
+    )
+}
+
+export default ScorePage
